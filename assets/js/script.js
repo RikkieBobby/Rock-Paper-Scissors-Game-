bba69f8fb0d1534e877b12b8f8d9041be4db81da
richie nolan <richienolan23@gmail.com>
2023-12-06 17:48:59 +0000

    add computer messages and input functions to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,15 @@ const playerChoiceBox = document.getElementById("player-choice");
 const computerChoiceBox = document.getElementById("computer-choice");
 const messages = document.getElementById("game-info");
 let playerChoice;
+let computerChoice;
 let playerMessage;
+let computerMessage;
 
 buttons.forEach(button => button.addEventListener('click', (e) => {
     playerChoice = e.target.id;
     playerInput();
+    setTimeout(computerMessages, 2000);
+    setTimeout(computerInput, 4000);
 }));
 
 function playerInput() {
@@ -26,6 +30,35 @@ function playerInput() {
     }
 
     messages.innerHTML = playerMessage;
+}
+
+function computerMessages() {
+
+    computerMessage = "The Computer is deciding...";
+    messages.innerHTML = computerMessage;
+
+}
+
+function computerInput() {
+
+    const randomNumber = Math.floor(Math.random() * 3) + 1;
+
+    if (randomNumber === 1) {
+        computerChoice = "Rock";
+        messages.innerHTML = `The Computer chose ${computerChoice}`;
+        // displayComputerRock();
+    }
+    if (randomNumber === 2) {
+        computerChoice = "Paper";
+        messages.innerHTML = `The Computer chose ${computerChoice}`;
+        // displayComputerPaper();
+    }
+    if (randomNumber === 3) {
+        computerChoice = "Scissors";
+        messages.innerHTML = `The Computer chose ${computerChoice}`;
+        // displayComputerScissors();
+    }
+
 }
 
 function displayPlayerRock() {
